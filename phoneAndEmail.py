8be82c1c8e4a25c6dee471504e8e6beab9ce3301
jdmax15 <joel.maxwell15@gmail.com>
2024-10-21 09:07:22 +1030

    added regex pattern for finidng phond numbers

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -3,3 +3,21 @@
 2. Find all phone numbers and email addresses in the text.
 3. paste them onto the clipboard
 '''
+
+#! python3
+# phoneAndEmail.py - Finds phone numbers and email address on the clipboard.
+
+import pyperclip, re
+phoneRegex = re.compile(r'''(
+                        (\d{3}|\(\d{3}\))?              # area code
+                        (\s|-|\.)?                      # separator
+                        (\d{3})                         # first 3 digits
+                        (\s|-|\.)                       # separator
+                        (\s*(ext|x|ext.)\s*(\d{2,5}))?  # last 4 digits
+                        )''', re.VERBOSE)
+
+# TODO: Create email regex.
+
+# TODO: Find matches in clipboard text.
+
+# TODO: Copy results to the clipboard.
